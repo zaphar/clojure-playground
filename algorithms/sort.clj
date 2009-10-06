fbde65974949cfd0a640d6ad1889a1cc4cd1a211
Jeremy Wall <jeremy@marzhillstudios.com>
2009-10-05 21:56:40 -0500

    working merge-sort

--- algorithms/sort.clj
+++ algorithms/sort.clj
@@ -24,8 +24,7 @@
 (defn insertion-sort [pred s]
   (do-insertion-sort 1 pred s))
 
-(defn merge-seq [acc pred s1 s2]
-  "merge two sorted sequences"
+(defn- do-merge-seq [acc pred s1 s2]
   (if (or (empty? s1) (empty? s2)) ; are either lists empty?
     (if (empty? s1) ; which one is empty?
       (concat acc s2) ; s1 wasn't empty so s2 must be empty
@@ -33,10 +32,14 @@
     (let [h1 (first s1)
           h2 (first s2)]
       (if (pred h1 h2)
-        (let [new-acc (cons h1 acc)]
+        (let [new-acc (concat acc (list h1))]
           (recur new-acc pred (drop 1 s1) s2))
-        (let [new-acc (cons h2 acc)]
+        (let [new-acc (concat acc (list h2))]
           (recur new-acc pred (drop 1 s2) s1))))))
+
+(defn merge-seq [pred s1 s2]
+  "merge two sorted sequences"
+  (do-merge-seq '() pred s1 s2))
 
 (defn merge-sort
   "merge-sort algorithm"
@@ -47,10 +50,10 @@
             lst2 (drop (int(/ seq-size 2)) s)]
         ; TODO(jwall): detect the leaf case
         (if (and (<= (count lst1) 1) (<= (count lst2) 1))
-          (merge-seq () pred lst1 lst2)
+          (merge-seq pred lst1 lst2)
           (let [new-lst1 (merge-sort pred lst1) ; sort lst1 recursively
                 new-lst2 (merge-sort pred lst2)] ; sort lst2 recursively
-            (merge-seq () pred new-lst1 new-lst2)))))))
+            (merge-seq pred new-lst1 new-lst2)))))))
 
 ; benchmarking code
 (defn time-insertion-sort [n]
