828c30fff2ecadbac30f27d79188f04041d490d2
Jeremy Wall <jeremy@marzhillstudios.com>
2009-11-21 10:03:04 -0600

    renamed the project target

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject com.marzhillstudios.test-tap "0.01.0"
+(defproject com.marzhillstudios.jwall-utils "0.01.0"
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]])
+
