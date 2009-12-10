4440d01a9d1ac5c527e5a769db3e4844f9ea3a80
Jeremy Wall <jeremy@marzhillstudios.com>
2009-12-09 20:39:15 -0600

    repeated token matcher

--- src/com/marzhillstudios/parse_utils.clj
+++ src/com/marzhillstudios/parse_utils.clj
@@ -3,18 +3,27 @@
   (:gen-class)
   (:use [com.marzhillstudios.test.tap :only [test-tap is ok]]))
 
-(defn any [matcher-seq] (partial any-of matcher-seq))
 (defn- any-of
   "Function that returns the first match of any of the provided matchers."
-  ([matcher-seq s]
-   (let [candidate ((first matcher-seq) s)]
-     (cond (nil? candidate) (recur (drop 1 matcher-seq) s)
+  ([matchers s]
+   (let [candidate ((first matchers) s)]
+     (cond (nil? candidate) (recur (drop 1 matchers) s)
        :else candidate))))
+(defn any [& matchers] (fn [s] (any-of matchers s)))
 
-(defn- repeated [matcher s]
-  ())
+(defn- repeated
+  ([acc matcher s]
+    (let [matched (matcher s)]
+      (cond (nil? matched) {:token (:token acc) :rest s}
+        :else (repeated {:token (conj (:token acc) (:token matched))
+                          :rest ""}
+                          matcher (:rest matched)))))
+  ([matcher s]
+    (let [matched (matcher s)]
+      (cond (nil? matched) nil
+        :else (repeated {:token [(:token matched)] :rest ""}
+                          matcher (:rest matched))))))
 
-(defn exact [token] (partial exact-token-maybe token))
 (defn- exact-token-maybe
   "Function that reads a token from a sequence.
    Returns token and rest of seq if matched, nil if no match."
@@ -27,8 +36,8 @@
              (cond
                (= sc tc) (recur (str acc sc) (drop 1 token) (drop 1 s))
                :else nil)))))
+(defn exact [token] (partial exact-token-maybe token))
 
-(defn until [token] (partial until-token-maybe token))
 (defn- until-token-maybe
   "Function that reads up to a token from a sequence.
    Returns portion of the sequence that matched. nil if no match."
@@ -39,6 +48,7 @@
                                           token (drop 1 s))
                      :else {:token (str acc (:token token?))
                             :rest (:rest token?)}))))
+(defn until [token] (partial until-token-maybe token))
 
 (defn test-suite []
   (test-tap 4
@@ -52,8 +62,11 @@
                          (exact "foo")]
                         "foo bar"))
             (is {:token "foo bar" :rest ()}
-                (any-of [(exact "baz")
-                         (until "bar")
-                         (exact "foo")]
-                        "foo bar"))
+                ((any (exact "baz")
+                      (until "bar")
+                      (exact "foo"))
+                       "foo bar"))
+            (is {:token ["foo" "foo"] :rest (seq " bar")}
+                (repeated (exact "foo") "foofoo bar"))
             ))
+
